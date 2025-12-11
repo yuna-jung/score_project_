09dd38d3395cbfdf83e479350f2fa6065c6f2cd2
yunajung224 <qpla2424@gmail.com>
2025-12-12 00:01:42 +0900

    aindocs: 주석 추가

--- bin/score_advanced_1.dart
+++ bin/score_advanced_1.dart
@@ -1,50 +1,50 @@
 import 'dart:io';
 
-class Score {
+class Score { //부모 class인 Score 지정
   int score=0;
-  void show(){
+  void show(){ //점수를 출력하는 기본 메서드
     print("점수는: $score");
   }
 }
 
-class StudentScore extends Score {
+class StudentScore extends Score { //클래스를 상속받아 학생 점수 클래스 정의
    String name='';
-    StudentScore(this.name, int score) {
+
+    StudentScore(this.name, int score) { //학생 정보와 점수를 문자열로 반환하는 메서드
       this.score=score;
     }
     String getResultString(){
       return "이름: $name, 점수: $score 점";
     }
 
-  @override
+  @override //Score 클래스의 show 메서드를 재정의
   void show(){
     print(getResultString());
   }
 }
 
-void saveResults(String filepath, String content){
+void saveResults(String filepath, String content){ //파일에 문자열을 저장하는 함수
     try{
-    final file=File(filepath);
-    file.writeAsStringSync(content);
+    final file=File(filepath); //저장할 파일 객체 생성
+    file.writeAsStringSync(content); //content 내용을 파일에 동기적으로 작성
     print("저장 완료");
     } catch(e){
-      print("저장 실패 $e");
+      print("저장 실패 $e"); // 오류 발생 시 메시지 출력
     }
   }
 
 void main(){
-    List<StudentScore>students=[];
+    List<StudentScore>students=[]; //학생 정보를 저장할 리스트 생성
 
   final file = File('students.txt');
-  final lines=file.readAsLinesSync();
+  final lines=file.readAsLinesSync(); //파일 내용을 줄 단위로 읽어 리스트로 반환
 
   for(var line in lines){
-    final parts=line.split(',');
-    String name=parts[0].trim();
+    final parts=line.split(','); //이름과 점수를 분리
+    String name=parts[0].trim(); //이름 부분 추출 후 공백 제거
     int score=int.parse(parts[1]);
 
     students.add(StudentScore(name,score));
-    // print("이름: $name, 점수: $score 점");
     
   }
   while(true){
